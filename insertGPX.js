54392d8a1a99d74970750039d38f7010cd3a1821
jsyang <jsyang.ca@gmail.com>
2016-06-30 23:58:28 +0100

    functional

--- insertGPX.js
+++ insertGPX.js
@@ -1,5 +1,87 @@
+/*
+
+ To Run:
+ ```
+    node insertGPX.js <your_gpx_file>
+ ```
+*/
+
+/*
+A GPX file is an XML file. Link to full spec here:
+https://en.wikipedia.org/wiki/GPS_Exchange_Format
+*/
+
+var execSync = require('child_process').execSync;
 var fs = require('fs');
 var cheerio = require('cheerio');
-var $ = cheerio.load(fs.readFileSync('routing_points.xml'));
 
-console.log($('routing_points').find('set').first().html());
+// http://stackoverflow.com/questions/31574127/node-js-cheerio-parser-brakes-uft-8-encoding
+var CHEERIO_OPTIONS = { decodeEntities: false };
+
+var XML$ = cheerio.load(fs.readFileSync('routing_points.xml'), CHEERIO_OPTIONS);
+var GPX$ = cheerio.load(fs.readFileSync(process.argv[2]), CHEERIO_OPTIONS);
+
+// Backup
+execSync('cp -f routing_points.xml routing_points.bak.xml');
+
+// Convert LatLng as decimal to LatLng as milliseconds
+function DEC_to_MS(deg) {
+    return Math.round(deg * 60 * 60 * 1000);
+}
+
+// Create new doc to be appended
+var newSet$ = cheerio.load([
+    '<set>',
+    '<name></name>',
+    '</set>'
+].join(''));
+
+var $set = newSet$('set');
+
+// Update set name
+var name = GPX$('trk').find('name').text();
+$set.find('name').html(name);
+
+// Add way points
+var $trkpt = GPX$('trkpt');
+var lastTrackpointIndex = $trkpt.length - 1;
+
+var trackXML = $trkpt.map(function addTrackPointAsWayPoint(i) {
+    var $this = GPX$(this);
+    var lat = DEC_to_MS(parseFloat($this.attr('lat')));
+    var lon = DEC_to_MS(parseFloat($this.attr('lon')));
+
+    if (i === 0) {
+        return [
+            '<departure>',
+            '<name>Start</name>',
+            '<lat>' + lat + '</lat>',
+            '<lon>' + lon + '</lon>',
+            '</departure>'
+        ].join('');
+    } else if (i === lastTrackpointIndex) {
+        return [
+            '<destination>',
+            '<name>End</name>',
+            '<lat>' + lat + '</lat>',
+            '<lon>' + lon + '</lon>',
+            '</destination>'
+        ].join('');
+    } else {
+        return [
+            '<waypoint>',
+            '<name>Waypoint ' + i + '</name>',
+            '<lat>' + lat + '</lat>',
+            '<lon>' + lon + '</lon>',
+            '</waypoint>'
+        ].join('');
+    }
+}).get();
+
+$set.append(trackXML.join('\n'));
+
+XML$('routing_points').append($set);
+
+// Write the new routing_points.xml
+
+fs.writeFileSync('routing_points.xml', XML$.xml());
--- readme.md
+++ readme.md
@@ -1,5 +1,26 @@
 # Add GPX to routing_points.xml
 
+This script inserts a track defined by a GPX file into the `routing_points.xml` file used by
+MapFactor Navigation so you can create a route and use it in the app. You will need to replace
+the `routing_points.xml` in your install directory on your device to see the added track.
+
+As [Route-Importer for MapFactor](https://play.google.com/store/apps/details?id=org.kadiba.routeimporter&hl=en_GB)
+failed to work on my Android Marshmellow device, I had to resort to writing my own GPX convertor for
+long cycle rides.
+
+## To Run
+
+`node insertGPX.js <your_gpx_file>`
+
+
+Example usage:
+
+```bash
+node insertGPX.js brouter-leicester-cardiff.gpx
+```
+
+## Notes
+
 Lat-long coordinates from GPX are in Decimal format vs Degrees Minutes Seconds (Milliseconds)
 format in the `routing_points.xml` file.
 http://stackoverflow.com/questions/17311316/convert-between-degree-and-miliseconds
@@ -17,3 +38,7 @@ function DEC_to_MS(deg){
 }
 
 ```
+
+## License
+
+This software is licensed under GPLv2. See LICENCE file for details.
